62af074c8d70c1fd8ddfc840f471c0a5c9259f58
_ＰＲＯＴＩＫ_/ <89929703+P-R-O-T-I-K@users.noreply.github.com>
2021-09-30 21:46:55 +0600

    fixed

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const deg = 6;
-const secondHand = document.querySelector('.sc');
-const minsHand = document.querySelector('.mn');
-const hourHand = document.querySelector('.hr');
+const secondHand = document.querySelector('.sec');
+const minsHand = document.querySelector('.min');
+const hourHand = document.querySelector('.hour');
 
 setInterval(() => {
     const now = new Date();
 
-    const hours = now.getHours() * 30;
-    const mins = now.getMinutes() * deg;
-    const seconds = now.getSeconds() * deg;
+    const hours = now.getHours() * 30 ;
+    const mins = now.getMinutes() * deg ;
+    const seconds = now.getSeconds() * deg ;
 
     const hoursDegree = (hours) + (mins / 12);
 
